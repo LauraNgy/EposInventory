19867ea8820b82bcd75b60ae564455512d74d184
WillNorval <w.norval1@gmail.com>
2018-11-03 23:54:37 +0000

    refactors Markup as read-only

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+
 
 namespace EposInventory.Models
 {
@@ -10,11 +13,25 @@ namespace EposInventory.Models
         public int ID { get; set; }
         public int ProviderID { get; set; }
         public string Description { get; set; }
-        public int PricePerUnit { get; set; }
-        public int SellingPrice { get;  set; }
-        public int Markup { get; set; }
+        [Display(Name = "Price Per Unit")]
+        public float PricePerUnit { get; set; }
+        [Display(Name = "Selling Price")]
+        public float SellingPrice { get;  set; }
+        [Display(Name = "Quantity in Stock")]
         public int Stock { get; set; }
+        [Display(Name = "Warning Level")]
         public int WarningLevel { get; set; }
+
+        [Display(Name = "Markup")]
+        public int Markup
+        {
+            get
+            {
+                var selling = Math.Round(SellingPrice, 2);
+                var ppu = Math.Round(PricePerUnit, 2);
+                return (int)((1-ppu/selling)*100);
+            }
+        }
 
         public virtual Provider Provider { get; set; }
     }
