8ac28e9a4cb5be258f2fcb0d588eed4a479d71e6
WillNorval <w.norval1@gmail.com>
2018-11-03 18:11:28 +0000

    adds Item and Provider models

---
+++ Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EposInventory.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public int ProviderID { get; set; }
+        public string Description { get; set; }
+        public int PricePerUnit { get; set; }
+        public int SellingPrice { get;  set; }
+        public int Markup { get; set; }
+        public int Stock { get; set; }
+        public int WarningLevel { get; set; }
+
+        public virtual Provider Provider { get; set; }
+    }
+}
---
+++ Models/Provider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EposInventory.Models
+{
+
+    public enum Day
+    {
+        Mon, Tue, Wed, Thu, Fri, Sat, Sun
+    }
+
+    public class Provider
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<Day> OrderDays { get; set; }
+        public ICollection<Day> DeliveryDays { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
