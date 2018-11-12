c5a481fef3132342d4c4c999da983f182a0abf86
LauraNagy <ngylaura@gmail.com>
2018-11-12 16:14:16 +0000

    introduces React

---
+++ ReactScripts/App.jsx
@@ -0,0 +1,18 @@
+﻿
+class App extends React.Component {
+    render() {
+        return (
+            <CardList />
+        );
+    }
+}
+
+class CardList extends React.Component {
+    render() {
+        return(
+           <p>It's something</p> 
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('content'));
